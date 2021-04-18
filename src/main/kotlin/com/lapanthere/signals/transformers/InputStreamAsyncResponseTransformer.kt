9013522d647340e2a74ae92817767a7130745518
Timothée Peignier <timothee.peignier@icloud.com>
2021-04-17 18:11:34 -0700

    Improve transformer

--- src/main/kotlin/com/lapanthere/signals/transformers/InputStreamAsyncResponseTransformer.kt
+++ src/main/kotlin/com/lapanthere/signals/transformers/InputStreamAsyncResponseTransformer.kt
@@ -2,6 +2,8 @@ package com.lapanthere.signals.transformers
 
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onCompletion
 import kotlinx.coroutines.flow.onEach
@@ -9,35 +11,108 @@ import kotlinx.coroutines.reactive.asFlow
 import software.amazon.awssdk.core.async.AsyncResponseTransformer
 import software.amazon.awssdk.core.async.SdkPublisher
 import software.amazon.awssdk.services.s3.model.GetObjectResponse
+import java.io.IOException
 import java.io.InputStream
 import java.io.PipedInputStream
 import java.io.PipedOutputStream
 import java.nio.ByteBuffer
+import java.util.Arrays
 import java.util.concurrent.CompletableFuture
 import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.cancellation.CancellationException
 
-public class InputStreamAsyncResponseTransformer :
-    AsyncResponseTransformer<GetObjectResponse, InputStream>,
-    CoroutineScope {
+public class InputStreamAsyncResponseTransformer(
+    override val coroutineContext: CoroutineContext = Dispatchers.IO
+) : AsyncResponseTransformer<GetObjectResponse, InputStream>, CoroutineScope {
     private val future = CompletableFuture<InputStream>()
     private val pipe = PipedOutputStream()
+    private lateinit var pipeline: Job
 
     override fun prepare(): CompletableFuture<InputStream> = future
 
     override fun onResponse(response: GetObjectResponse) {}
 
     override fun onStream(publisher: SdkPublisher<ByteBuffer>) {
-        future.complete(PipedInputStream(pipe))
-        publisher.asFlow()
-            .onEach { pipe.write(it.array()) }
-            .onCompletion { pipe.close() }
+        val inputStream = CancellablePipedInputStream(pipe)
+        pipeline = publisher.asFlow()
+            .onEach { pipe.write(it.toByteArray()) }
+            .catch { inputStream.cancel(it) }
+            .onCompletion {
+                pipe.flush()
+                pipe.close()
+            }
             .launchIn(this)
+        future.complete(inputStream)
     }
 
     override fun exceptionOccurred(error: Throwable) {
         future.completeExceptionally(error)
+        if (::pipeline.isInitialized) {
+            pipeline.cancel(CancellationException(cause = error))
+        }
+    }
+}
+
+internal class CancellablePipedInputStream(
+    private val inputStream: PipedInputStream
+) : InputStream() {
+    constructor(pipedOutputStream: PipedOutputStream) : this(PipedInputStream(pipedOutputStream))
+
+    private var cancellationException: Throwable? = null
+
+    internal fun cancel(throwable: Throwable) {
+        cancellationException = throwable
     }
 
-    override val coroutineContext: CoroutineContext
-        get() = Dispatchers.IO
+    private inline fun <T> run(block: () -> T): T {
+        if (cancellationException != null) {
+            throw IOException(cancellationException)
+        }
+        return block()
+    }
+
+    private inline fun finally(block: () -> Unit) {
+        block()
+        if (cancellationException != null) {
+            throw IOException(cancellationException)
+        }
+    }
+
+    override fun available(): Int = run {
+        inputStream.available()
+    }
+
+    override fun read(b: ByteArray): Int = run {
+        inputStream.read(b)
+    }
+
+    override fun read(b: ByteArray, off: Int, len: Int): Int = run {
+        inputStream.read(b, off, len)
+    }
+
+    override fun read(): Int = run {
+        inputStream.read()
+    }
+
+    override fun skip(n: Long): Long = run {
+        inputStream.skip(n)
+    }
+
+    override fun close() = finally {
+        inputStream.close()
+    }
+}
+
+internal fun ByteBuffer.toByteArray(): ByteArray {
+    if (hasArray()) {
+        return Arrays.copyOfRange(
+            array(),
+            arrayOffset() + position(),
+            arrayOffset() + limit()
+        )
+    }
+
+    val bytes = ByteArray(remaining())
+    asReadOnlyBuffer().get(bytes)
+    return bytes
 }
---
+++ src/test/kotlin/com/lapanthere/signals/transformers/InputStreamAsyncResponseTransformerTest.kt
@@ -0,0 +1,71 @@
+package com.lapanthere.signals.transformers
+
+import org.reactivestreams.Subscriber
+import org.reactivestreams.Subscription
+import software.amazon.awssdk.services.s3.model.GetObjectResponse
+import java.io.IOException
+import java.io.OutputStream
+import java.nio.ByteBuffer
+import kotlin.random.Random
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
+import kotlin.test.assertTrue
+
+internal class InputStreamAsyncResponseTransformerTest {
+    @Test
+    fun `stream error are propagated`() {
+        val transformer = InputStreamAsyncResponseTransformer()
+        val future = transformer.prepare()
+        transformer.onResponse(GetObjectResponse.builder().build())
+        transformer.onStream { subscriber: Subscriber<in ByteBuffer> ->
+            subscriber.onSubscribe(object : Subscription {
+                override fun request(l: Long) {}
+
+                override fun cancel() {}
+            })
+            subscriber.onError(RuntimeException("unexpected exception"))
+        }
+        val inputStream = future.get()
+        assertFailsWith(IOException::class) {
+            inputStream.use {
+                it.copyTo(nullOutputStream)
+            }
+        }
+    }
+
+    @Test
+    fun `completes exceptionally if request failed`() {
+        val transformer = InputStreamAsyncResponseTransformer()
+        val future = transformer.prepare()
+        transformer.exceptionOccurred(RuntimeException("unexpected exception"))
+        assertTrue(future.isCompletedExceptionally)
+    }
+
+    @Test
+    fun `propagates bytes to the reader`() {
+        val transformer = InputStreamAsyncResponseTransformer()
+        val future = transformer.prepare()
+        transformer.onResponse(GetObjectResponse.builder().build())
+        transformer.onStream { subscriber: Subscriber<in ByteBuffer> ->
+            subscriber.onSubscribe(object : Subscription {
+                override fun request(l: Long) {
+                    subscriber.onNext(ByteBuffer.wrap(Random.nextBytes(30_000)))
+                    subscriber.onComplete()
+                }
+
+                override fun cancel() {}
+            })
+        }
+        val inputStream = future.get()
+        inputStream.use {
+            val bytes = it.readBytes()
+            assertTrue(bytes.isNotEmpty())
+            assertEquals(30_000, bytes.size)
+        }
+    }
+}
+
+internal val nullOutputStream = object : OutputStream() {
+    override fun write(b: Int) {}
+}
